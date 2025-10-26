2b57bbb34fce5e96ed7347a712f3488ec38f0fda
sajid <sajidaliacademics@gmail.com>
2025-10-26 09:52:07 +0530

    wreek text

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const socialHam = document.querySelector('.social-ham');
 hamburger.addEventListener('click', () => {
     socialHam.classList.toggle('show');
 });
+
+const textContainers = document.querySelectorAll('.text-container');
+
+  textContainers.forEach((box, index) => {
+    box.setAttribute('data-scroll', '');
+    box.setAttribute('data-scroll-speed', (index % 2 === 0) ? '1.2' : '2'); 
+    box.setAttribute('data-scroll-delay', '0.1');
+  });
+  
+  scroll.update(); // refresh locomotive after changes
